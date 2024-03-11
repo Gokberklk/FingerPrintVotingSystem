6d70db745af31960735909128b8c3363ba59649a
CanberkSefa <65313912+CanberkSefa@users.noreply.github.com>
2024-03-12 00:06:10 +0200

    Admin page is added and the admin can remove elections from the database.

--- admin.py
+++ admin.py
@@ -4,14 +4,54 @@ import fingerprintVotingSystem
 
 app = Flask(__name__)
 
+elections = None
+
+@app.route('/login', methods=['POST','GET'])
+def Login():#This function is used for the login operation of the admin.
+
+    return
 
 @app.route("/", methods=['POST', 'GET'])
-def AdminMainPage():
+def AdminMainPage():#This function is for main page after a succesful login operation of the admin in to the system.
 
-    return render_template("adminLayout.html")
+    return render_template("admin_main.html")
 
-@app.route("/index", methods=['POST', 'GET'])
-def index():
-    return render_template("index.html")
+@app.route("/elections", methods=['GET'])
+def Elections():#This function is used to call the page in which the admin see elections.
+
+    #In this part, all elections from the database are fetched to be shown in the elections page of the admin.
+    global elections
+    connectionDB = sqlite3.connect("Government")
+    cursor = connectionDB.cursor()
+    cursor.execute("SELECT * FROM Election")
+    elections = cursor.fetchall()
+    cursor.close()
+    connectionDB.close()
+
+
+    return render_template("elections.html", elections=elections) #Fetched elections are send as a parameter.
+
+@app.route("/elections", methods=['POST','GET']) #
+def RemoveElection():#This function deletes the selected election from the database.
+    global elections
+
+    electionID = request.form.get('ElectionID')
+    connectionDB = sqlite3.connect("Government")
+    cursor = connectionDB.cursor()
+    cursor.execute("DELETE FROM Election WHERE electionID = ?", (electionID,))
+    cursor.execute("SELECT * FROM Election")
+    elections = cursor.fetchall()#Fetching new elections list from the database to show it.
+    connectionDB.commit()
+    cursor.close()
+    connectionDB.close()
+
+    return render_template("elections.html",elections=elections)
+
+
+
+@app.route("/candidates", methods=['GET'])
+def Candidates():#This function is used to call the page in which the admin see candidates.
+    return render_template("candidates.html")
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
