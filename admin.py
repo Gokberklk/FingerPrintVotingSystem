15e0367d79a840dac3063c0bbd0af14b07495db9
Adem-grp <109536482+Adem-grp@users.noreply.github.com>
2024-05-11 16:50:50 +0300

    session finished
    session finished / cache always gets cleared

--- admin.py
+++ admin.py
@@ -2,20 +2,46 @@ from flask import *
 import sqlite3
 import fingerprintVotingSystem
 import base64
+from functools import wraps
+from flask import session, redirect
+
 app = Flask(__name__)
-app.secret_key="KawakiWoAmeku"
+app.secret_key = "KawakiWoAmeku"
 elections = None
 candidates = None
 
 
-@app.route('/logout', methods=['POST','GET'])
-def LogOut():#This function is used to log out.
+def check_authentication(view_func):
+    @wraps(view_func)
+    def wrapped_function(*args, **kwargs):
+        if 'voter' not in session:
+            return redirect('/')
+        return view_func(*args, **kwargs)
+
+    return wrapped_function
+
+
+def add_no_cache_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+
+@app.after_request
+def apply_no_cache(response):
+    return add_no_cache_headers(response)
+
+
+@app.route('/logout', methods=['POST', 'GET'])
+def LogOut():  # This function is used to log out.
     if 'adminID' in session:
         del session['adminID']
     return render_template("adminLogin.html")
 
-@app.route('/', methods=['POST','GET'])
-def Login():#This function is used for the selectiong operation of the admin.
+
+@app.route('/', methods=['POST', 'GET'])
+def Login():  # This function is used for the selectiong operation of the admin.
 
     if request.method == 'GET':
         return render_template("adminLogin.html")
@@ -31,7 +57,7 @@ def Login():#This function is used for the selectiong operation of the admin.
         connectionDB.close()
 
         if tempAdminID is None:
-            return render_template("adminLogin.html",error="Admin does not exist!")
+            return render_template("adminLogin.html", error="Admin does not exist!")
         else:
             if adminID not in session:
                 session['adminID'] = adminID
@@ -40,17 +66,21 @@ def Login():#This function is used for the selectiong operation of the admin.
 
 
 @app.route("/operation", methods=['POST', 'GET'])
-def AdminMainPage():#This function is for main page after a succesful login operation of the admin in to the system.
+@check_authentication
+def AdminMainPage():  # This function is for main page after a succesful login operation of the admin in to the system.
 
     if 'adminID' in session:
         return render_template("admin_main.html")
     else:
         return redirect(url_for('Login'))
 
-@app.route("/elections", methods=['GET'])
-def Elections():#This function is used to call the page in which the admin see elections.
 
-    #In this part, all elections from the database are fetched to be shown in the elections page of the admin.
+@app.route("/elections", methods=['GET'])
+@check_authentication
+def Elections():  # This function is used to call the page in which the admin see elections.
+    if request.method == 'GET':
+        return redirect("/")
+    # In this part, all elections from the database are fetched to be shown in the elections page of the admin.
     global elections
     connectionDB = sqlite3.connect("Government")
     cursor = connectionDB.cursor()
@@ -59,57 +89,63 @@ def Elections():#This function is used to call the page in which the admin see e
     cursor.close()
     connectionDB.close()
 
+    return render_template("elections.html", elections=elections)  # Fetched elections are send as a parameter.
 
-    return render_template("elections.html", elections=elections) #Fetched elections are send as a parameter.
 
-@app.route("/elections", methods=['POST','GET']) #
-def RemoveElection():#This function deletes the selected election from the database.
+@app.route("/elections", methods=['POST', 'GET'])  #
+@check_authentication
+def RemoveElection():  # This function deletes the selected election from the database.
     global elections
-
+    if request.method == 'GET':
+        return redirect("/")
     electionID = request.form.get('ElectionID')
     connectionDB = sqlite3.connect("Government")
     cursor = connectionDB.cursor()
     cursor.execute("DELETE FROM Election WHERE electionID = ?", (electionID,))
     cursor.execute("SELECT * FROM Election")
-    elections = cursor.fetchall()#Fetching new elections list from the database to show it.
+    elections = cursor.fetchall()  # Fetching new elections list from the database to show it.
     connectionDB.commit()
     cursor.close()
     connectionDB.close()
 
-    return render_template("elections.html",elections=elections)
+    return render_template("elections.html", elections=elections)
 
 
-@app.route("/elections/add", methods=['POST','GET']) #
-def AddElection():#If the request is POST, this
+@app.route("/elections/add", methods=['POST', 'GET'])  #
+@check_authentication
+def AddElection():  # If the request is POST, this
     # function retrieves election data from admin and add the election to the database. Otherwise
     # it shows add election page to the admin.
 
-    if request.method == 'GET': #Election adding page is shown.
+    if request.method == 'GET':  # Election adding page is shown.
         return render_template("addElection.html")
 
-    else: #Retrieve information of the election and add it to the database.
+    else:  # Retrieve information of the election and add it to the database.
         description = request.form.get('description')
         date = request.form.get('date')
         time = request.form.get('time')
         endDate = request.form.get('endDate')
         endTime = request.form.get('endTime')
 
-
-        #Adding the election to the database after checking validity of the data retrieved.
+        # Adding the election to the database after checking validity of the data retrieved.
         connectionDB = sqlite3.connect("Government")
         cursor = connectionDB.cursor()
-        cursor.execute("INSERT INTO Election (Result,DateOfElection,ElectionTime,Description,EndDate,EndTime) VALUES(?, ?, ?, ?, ?, ?)", (None,date,time,description,endDate,endTime))
+        cursor.execute(
+            "INSERT INTO Election (Result,DateOfElection,ElectionTime,Description,EndDate,EndTime) VALUES(?, ?, ?, ?, ?, ?)",
+            (None, date, time, description, endDate, endTime))
         connectionDB.commit()
         cursor.close()
         connectionDB.close()
 
         return redirect(url_for('Elections'))
 
-@app.route("/elections/update", methods=['POST','GET'])
+
+@app.route("/elections/update", methods=['POST', 'GET'])
+@check_authentication
 def UpdateElection():
-
     condition = request.args.get('value')
-
+    if request.method == 'GET':
+        return redirect("/")
     if request.method == 'POST' and condition == None:
 
         electionID = request.form.get('ElectionID')
@@ -126,7 +162,8 @@ def UpdateElection():
         cursor.close()
         connectionDB.close()
 
-        return render_template("updateElection.html",description=description,date=date,time=time,electionID=electionID,endDate=endDate,endTime=endTime)
+        return render_template("updateElection.html", description=description, date=date, time=time,
+                               electionID=electionID, endDate=endDate, endTime=endTime)
 
     else:
         description = request.form.get('description')
@@ -138,16 +175,21 @@ def UpdateElection():
 
         connectionDB = sqlite3.connect("Government")
         cursor = connectionDB.cursor()
-        cursor.execute("UPDATE Election SET Description = ?, DateOfElection = ?, ElectionTime = ?, EndDate = ?, EndTime = ? WHERE ElectionID = ?", (description, date, time,endDate,endTime,electionID))
+        cursor.execute(
+            "UPDATE Election SET Description = ?, DateOfElection = ?, ElectionTime = ?, EndDate = ?, EndTime = ? WHERE ElectionID = ?",
+            (description, date, time, endDate, endTime, electionID))
         connectionDB.commit()
         cursor.close()
         connectionDB.close()
 
         return redirect(url_for('Elections'))
 
-@app.route("/candidates", methods=['GET'])
-def Candidates():#This function is used to call the page in which the admin see candidates.
 
+@app.route("/candidates", methods=['GET', 'POST'])
+@check_authentication
+def Candidates():  # This function is used to call the page in which the admin see candidates.
+    if request.method == 'GET':
+        return redirect("/")
     # In this part, all elections from the database are fetched to be shown in the candidates page of the admin.
     global elections
     connectionDB = sqlite3.connect("Government")
@@ -157,7 +199,7 @@ def Candidates():#This function is used to call the page in which the admin see 
     cursor.close()
     connectionDB.close()
 
-    #Retrieving all candidates from the "Government" database.
+    # Retrieving all candidates from the "Government" database.
     # connectionDB = sqlite3.connect("Government")
     # cursor = connectionDB.cursor()
     # cursor.execute("SELECT * FROM Candidate")
@@ -169,15 +211,17 @@ def Candidates():#This function is used to call the page in which the admin see 
     return render_template("candidates.html", elections=elections)
 
 
-@app.route("/candidates", methods=['GET','POST'])
-def AddCandidate():#This function is used to call the page in which the admin see candidates.
-
+@app.route("/candidates", methods=['GET', 'POST'])
+@check_authentication
+def AddCandidate():  # This function is used to call the page in which the admin see candidates.
+    if request.method == 'GET':
+        return redirect("/")
     condition = request.args.get('value')
 
     if condition == '0':
         ElectionID = request.form.get('ElectionID')
 
-        return render_template("addCandidate.html",electionID=ElectionID)
+        return render_template("addCandidate.html", electionID=ElectionID)
 
     elif condition == '1':
 
@@ -186,32 +230,35 @@ def AddCandidate():#This function is used to call the page in which the admin se
 
         connectionDB = sqlite3.connect("Government")
         cursor = connectionDB.cursor()
-        cursor.execute("SELECT * FROM CandidateElection WHERE ElectionID = ? AND CitizenID= ?", (electionID,candidateID))
+        cursor.execute("SELECT * FROM CandidateElection WHERE ElectionID = ? AND CitizenID= ?",
+                       (electionID, candidateID))
         isExist = cursor.fetchone()
 
         if isExist == None:
             cursor.execute("SELECT * FROM Candidate WHERE CitizenID= ?",
                            (candidateID,))
-            tempCandidate=cursor.fetchone()
+            tempCandidate = cursor.fetchone()
 
             if tempCandidate is None:
-                cursor.execute("INSERT INTO Candidate (CitizenID) VALUES(?)",  (candidateID,))
+                cursor.execute("INSERT INTO Candidate (CitizenID) VALUES(?)", (candidateID,))
                 connectionDB.commit()
 
             cursor.execute(
                 "INSERT INTO CandidateElection (CountOfVote,CitizenID,ElectionID) VALUES(?,?,?)",
-                (0,candidateID,electionID))
+                (0, candidateID, electionID))
             connectionDB.commit()
 
             return redirect(url_for('Candidates'))
 
         else:
-            return render_template("addCandidate.html",error="Candidate already exists")
+            return render_template("addCandidate.html", error="Candidate already exists")
 
 
-@app.route("/candidates/remove", methods=['GET','POST'])
-def RemoveCandidate():#This function is used to call the page in which the admin see candidates.
-
+@app.route("/candidates/remove", methods=['GET', 'POST'])
+@check_authentication
+def RemoveCandidate():  # This function is used to call the page in which the admin see candidates.
+    if request.method == 'GET':
+        return redirect("/")
     condition = request.args.get('value')
 
     if condition == '0':
@@ -221,7 +268,6 @@ def RemoveCandidate():#This function is used to call the page in which the admin
         cursor.execute("SELECT * FROM CandidateElection WHERE ElectionID = ?",
                        (electionID,))
         candidateElections = cursor.fetchall()
-
 
         candidatesList = []
         image_base64 = []
@@ -236,7 +282,8 @@ def RemoveCandidate():#This function is used to call the page in which the admin
         cursor.close()
         connectionDB.close()
 
-        return render_template("removeCandidate.html",candidatesList=candidatesList,Image=image_base64,electionID=electionID)
+        return render_template("removeCandidate.html", candidatesList=candidatesList, Image=image_base64,
+                               electionID=electionID)
 
     elif condition == '1':
 
@@ -247,12 +294,13 @@ def RemoveCandidate():#This function is used to call the page in which the admin
         print(electionID)
         connectionDB = sqlite3.connect("Government")
         cursor = connectionDB.cursor()
-        cursor.execute("DELETE FROM CandidateElection WHERE CitizenID = ? and ElectionID = ?",(candidateID,electionID))
+        cursor.execute("DELETE FROM CandidateElection WHERE CitizenID = ? and ElectionID = ?",
+                       (candidateID, electionID))
         connectionDB.commit()
 
         tempCandidate = None
-        cursor.execute("SELECT * FROM CandidateElection WHERE CitizenID = ?",(candidateID,))
-        tempCandidate=cursor.fetchone()
+        cursor.execute("SELECT * FROM CandidateElection WHERE CitizenID = ?", (candidateID,))
+        tempCandidate = cursor.fetchone()
 
         print(tempCandidate)
 
@@ -262,6 +310,7 @@ def RemoveCandidate():#This function is used to call the page in which the admin
             connectionDB.commit()
 
         return redirect(url_for('Candidates'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
